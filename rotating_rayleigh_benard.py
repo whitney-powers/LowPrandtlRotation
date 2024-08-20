8a7167cef2ec1d714c7752a69596a005cff09931
Whitney Powers <whitneypowers@sedna-7.local>
2024-08-19 22:06:27 -0600

    switching to stress free boundary conditions

--- rotating_rayleigh_benard.py
+++ rotating_rayleigh_benard.py
@@ -105,14 +105,24 @@ Omega = 1/2*Taylor**(1/2) * (Rayleigh / Prandtl)**(-1/2) * ez
 # Problem
 # First-order form: "div(f)" becomes "trace(grad_f)"
 # First-order form: "lap(f)" becomes "div(grad_f)"
+
+trans = lambda A: d3.TransposeComponents(A)
+e = grad(u) + trans(grad(u))
 problem = d3.IVP([p, b, u, tau_p, tau_b1, tau_b2, tau_u1, tau_u2], namespace=locals())
 problem.add_equation("trace(grad_u) + tau_p = 0")
 problem.add_equation("dt(b) - kappa*div(grad_b) + lift(tau_b2 ) = - u@grad(b)")
 problem.add_equation("dt(u) - nu*div(grad_u) + grad(p) - b*ez + 2*cross(Omega, u) + lift(tau_u2) = - u@grad(u)")
 problem.add_equation("b(z=0) = Lz")
-problem.add_equation("u(z=0) = 0")
+#problem.add_equation("u(z=0) = 0")
+problem.add_equation('ez@u(z=0) = 0')
+problem.add_equation('ez@(ex@e(z=0)) = 0')
+problem.add_equation('ez@(ey@e(z=0)) = 0')
+
 problem.add_equation("b(z=Lz) = 0")
-problem.add_equation("u(z=Lz) = 0")
+#problem.add_equation("u(z=Lz) = 0")
+problem.add_equation('ez@u(z=Lz) = 0')
+problem.add_equation('ez@(ex@e(z=Lz)) = 0')
+problem.add_equation('ez@(ey@e(z=Lz)) = 0')
 problem.add_equation("integ(p) = 0") # Pressure gauge
 
 # Solver
